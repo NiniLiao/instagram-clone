7dba4cb187c3d260a101257b3ee76a0442ddcb30
Nini Liao <u9513031@cmu.edu.tw>
2022-06-02 19:30:37 +0800

    login.js added

---
+++ src/pages/login.js
@@ -0,0 +1,23 @@
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+   
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+      document.title = 'Login - Instagram';
+  }, []);
+
+  return <p>I am the login page</p>;
+
+}
