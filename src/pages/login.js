1b79665f2b6a32d95723a7ec786e1d9f6a3c895e
Nini Liao <u9513031@cmu.edu.tw>
2022-06-04 03:19:08 +0800

    added some classes

--- src/pages/login.js
+++ src/pages/login.js
@@ -19,8 +19,17 @@ export default function Login() {
   }, []);
 
   return (
-    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
-      <p>I have no idea!</p>
+    <div className='container flex mx-auto max-w-screen-md items-center'>
+      <div className="flex w-3/5">
+        <img 
+          src="/images/iphone-with-profile.jpg" 
+          alt="iPhone with Instagram app"
+          className='max-w-xs'  
+        />
+      </div>
+      <div className='flex flex-col w-2/5'>
+        <p>I will be the form!</p>
+      </div>
     </div>  
   );
 
