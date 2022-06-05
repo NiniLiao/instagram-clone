00b3b55c2ffc9df41ece6d3269d64f0852d8d20b
Nini Liao <u9513031@cmu.edu.tw>
2022-06-05 23:45:52 +0800

    removed navigate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import ('./pages/login'));
