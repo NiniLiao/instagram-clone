9c7b1ad29b7f17900ab7d64b179671d884566d51
Nini Liao <u9513031@cmu.edu.tw>
2022-06-06 12:00:10 +0800

    removed extra modules

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,12 @@
-import { func } from 'prop-types';
 import { firebase, FieldValue } from '../lib/firebase';
 
 export async function doesUsernameExist(username) {
     const result = await firebase
       .firestore()
       .collection('users')
-      .where('username', '==', username)
+      .where('username', '==', username.toLowerCase())
       .get();
     
-    return result.docs.map((user) => user.data().length > 0);
-
+    //return result.docs.map((user) => user.data().length > 0);
+    return result.docs.length > 0;
 }
