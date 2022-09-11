22e4e0d0c3aaaf7cdf18da4e880b5aae2f8d8787
Nini Liao <u9513031@cmu.edu.tw>
2022-09-12 00:12:23 +0800

    added Avatar

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,23 @@
 import React from "react";
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
   return (
-    <div>
-      <h3>Username</h3>
+    <div className="post">
+      <div className="post__header">
+        <Avatar 
+            className="post__avatar"
+            alt="{username}"
+            src="/static/images/avatar/1.jpg"
+        />      
+        <h3>Username</h3>
+      </div>
+      
+      <img className="post__image" src="https://i1.wp.com/popbee.com/image/2020/06/spongebob-squarepants-gay-nickelodeon-pride-month-teaser.jpg" />
+    
+      <h4 className="post__text"><strong>Username</strong> caption</h4>
+
     </div>
   )
 }
